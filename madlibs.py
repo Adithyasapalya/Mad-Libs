4644f47baa8a97376f9947b7102dc546c0fed7c9
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-08-17 14:21:10 +0530

    Initial Commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+adjectivel = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+print(f"Today I went to a {adjectivel} zoo. ")
+print(f"ln an exhibit, I saw {noun}" )
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"l was {adjective3}")
