1f49ad3c1895ac4998d38699a0368f504a2e7120
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-08-17 14:58:39 +0530

    U Commit

--- areacalc/areavcalc.py
+++
@@ -1,8 +0,0 @@
-# A Program to Calculate Area.
-
-length = float(input("Enter the length of rectangle: "))
-width = float(input("Enter the width of rectangle: "))
-height = float(input("Enter the height of rectangle: "))
-
-volume = length * width * height  
-print(f"The volume is : {volume}cm^3")
