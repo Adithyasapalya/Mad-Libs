1da6d013c503a0a9dcf96e8fb4c16a1453706a82
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-08-17 14:51:58 +0530

    Initial Commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# A simple Mad Libs game, where you can create a mini story using python and simple parts of speech.
-<br>
- Author: Adithya Ashok Sapalya
---
+++ areacalc/areacalc.py
@@ -0,0 +1,8 @@
+# A Program to Calculate Area.
+
+length = float(input("Enter the length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+height = float(input("Enter the height of rectangle: "))
+
+volume = length * width * height  
+print(f"The volume is : {volume}cm^3")
